fa136874db41c95912c1bb3f3309defd6e885767
Harry Park <hg98.park@hanwha.com>
2019-02-11 01:13:48 +0900

    make project with yoman

---
+++ hl/app/scripts/controllers/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hlApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hlApp
+ */
+angular.module('hlApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma',
+      'E2E',
+      'Protractor'
+    ];
+  });
