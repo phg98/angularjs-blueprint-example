e4abe491c9ffded9e5ce8c8d7369d423894ca229
Harry Park <hg98.park@hanwha.com>
2019-02-10 13:58:26 +0900

    address book

---
+++ script.js
@@ -0,0 +1,8 @@
+function PeopleController($scope) {
+    $scope.people = [
+        {name: "HG Park", phone: "12345678", city: "Seoul"},
+        {name: "John Doe", phone: "23467890", city: "New York"},
+        {name: "SY Ahn", phone: "12345909", city: "Ansan"},
+        
+    ]
+}
